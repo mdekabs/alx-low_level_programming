ef7cf0580f24f70d840aeb0f912d4672202caad2
mawoda <1mawoda@gmail.com>
2022-09-08 17:28:53 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - print the string
- *
- * Return: Always 0 (success) 
- */
-int main (void)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}	
+	    puts("\"Programming is like building a multilingual puzzle");
+	        return (0);
+}
