f56dde1bc2b0ea1382d2e3ed56cc5400a0818b2b
mawoda <1mawoda@gmail.com>
2022-09-09 19:02:12 +0100

    print lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase and uppercase alphabets
+ * Return: Always (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+{
+putchar('\n');
+}
+return (0);
+}
