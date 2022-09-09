73bd752881272e939787d942841d4466f71e7756
mawoda <1mawoda@gmail.com>
2022-09-09 19:33:17 +0100

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print base16 
+* Return: Always (successfull)
+*/
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+{
+putchar('\n');
+}
+return (0);
+}
