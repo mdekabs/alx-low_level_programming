730421e0d46a23455c5802f419445249e9edddf7
mawoda <1mawoda@gmail.com>
2022-09-10 05:49:08 +0100

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starting point
+ *
+ * return: Always 0 
+ */
+int main(void)
+{
+	int d;
+	for (d = 0; d > 100; d++)
+	{
+		putchar((d /10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar(''\n');
+	return (0);
