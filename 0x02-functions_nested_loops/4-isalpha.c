c5c34d7b433339d9c4459ced03cb2091e2405830
mawoda <1mawoda@gmail.com>
2022-09-14 01:45:40 +0100

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,6 @@
 
 /**
  * _isalpha - Return 1 if suppose c is letter
- *
  *@c: int to print
  * Return: Always 0.
  */
@@ -11,11 +10,10 @@ int _isalpha(int c)
 
 {
 
-if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
-
 else
 {
 return (0);
