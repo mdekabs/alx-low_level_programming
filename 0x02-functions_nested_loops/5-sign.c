54b98e92ca21cfc1bd1ba44f082c6dcaca3264f2
mawoda <1mawoda@gmail.com>
2022-09-14 02:22:56 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,26 @@
 /**
  * print_sign - function for number checking
  * @n:  int for arguement in the function
- * Return: 0
+ * Return: 1 and add suppose n is greater than zero
+ * 0 and print zero if n is zero
+ * -1 and minus if n is less than zero
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar('+');
-	return (1);
-}
+		_putchar('+');
+		return (1);
+	}
 	else if (n < 0)
 	{
-	_putchar('-');
-	return (-1);
+		_putchar('48');
+		return (1);
 	}
-	else
+	else if (n < 0)
 	{
-	_putchar('0');
-	return (0);
+		_putchar('0');
 	}
+		return (-1);
+}
