2d3a6f2a2f3347b394f328fadaaf8072dbef2cd7
mawoda <1mawoda@gmail.com>
2022-09-15 16:21:47 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print - num 0 to 14 x10
+ *
+ * Return: none
+ */
+void more_numbers(void)
+{
+int i;
+int first_number;
+int second_number;
+int result;
+
+i = 0;
+result = 0;
+while (i < 10)
+{
+while (result <= 14)
+{
+if (result < 10)
+{
+second_number = result;
+}
+
+else
+{
+first_number = result / 10;
+second_number = result % 10;
+_putchar (first_number + '0');
+}
+
+_putchar (second_number + '0');
+
+result++;
+}
+i++;
+result = 0;
+_putchar ('\n');
+}
+
+}
