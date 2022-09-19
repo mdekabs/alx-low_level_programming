312c723e42b478e296049a7f5c5cd23c676f1921
mawoda <1mawoda@gmail.com>
2022-09-19 23:36:48 +0100

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,7 @@ void rev_string(char *s)
 	int i, len, len1;
 
 	len = 0;
+
 	len1 = 0;
 
 	while (s[len] != '\0')
@@ -22,7 +23,9 @@ void rev_string(char *s)
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
+
 		s[i] = s[len1];
+
 		s[len1--] = tmp;
 	}
 }
