2aa8838545e5519b8f837fa8e3b47bfc1c458770
mawoda <1mawoda@gmail.com>
2022-09-19 23:08:31 +0100

    swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int - swap the valuse of two ints a and b
- *@a: A pointer to an int that will 
+ *@a: A pointer to an int that will
  *@b: B pointer to an int that will be updated
  * Return: void shows its correct
  */
