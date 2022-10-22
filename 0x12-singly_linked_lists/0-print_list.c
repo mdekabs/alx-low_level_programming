3230017060ed00899626b6791e14586bd33cebba
mawoda <1mawoda@gmail.com>
2022-10-22 05:09:57 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nodes = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+	return (nodes);
 }
