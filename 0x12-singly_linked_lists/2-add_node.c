57c9204e5b091aa9f56fd91d36ffbe8a70d0227b
mawoda <1mawoda@gmail.com>
2022-10-22 05:25:29 +0100

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "list.h"
+#include <strlng.h>
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
