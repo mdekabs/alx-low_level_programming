be8dc240aa591688d021d6827a42d48455d424b0
mdekabs <1mawoda@gmail.com>
2023-01-06 16:36:06 +0100

    corrected

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t)); /* Dynamic memory allocation */
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
@@ -24,4 +24,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		ht->array[i] = NULL;
 
 	return (ht);
+	free(ht)
 }
